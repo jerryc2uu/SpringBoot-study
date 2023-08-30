4fedba752064892edcb00f7c799d620573ac24ba
aknod <aknod@psy>
2023-08-30 14:07:02 +0900

    [Spring] Spring Boot 시작하기 (3) - Lombok 사용 기본 (@Data)

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -3,10 +3,13 @@ package com.example.demo.controller;
 import java.util.*;
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -15,17 +18,40 @@ public class HomeController {
 	@RequestMapping(value = "/home", method=RequestMethod.GET)
 	public ModelAndView goHome(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
-		List<String> resultList = new ArrayList<String>();
+
+		//salary 객체 리스트 변수
+		List<SalaryModel> salaryList = new ArrayList<SalaryModel>();
 		
-		resultList.add("AAA");
-		resultList.add("BBB");
-		resultList.add("CCC");
-		resultList.add("DDD");
-		resultList.add("EEE");
-		resultList.add("FFF");
 		
-		mav.addObject("resultList",resultList); //데이터 담기
-		mav.setViewName("content/home"); //뷰 담기
+		//Salary model
+		SalaryModel salaryModel = new SalaryModel();
+		
+		// 첫 번째 데이터
+		salaryModel = new SalaryModel();
+		salaryModel.setId(1);
+		salaryModel.setName("AAA");
+		salaryModel.setEmail("AAA@example.com");		
+		salaryList.add(salaryModel);
+		
+		
+		// 두 번째 데이터
+		salaryModel = new SalaryModel();
+		salaryModel.setId(2);
+		salaryModel.setName("BBB");
+		salaryModel.setEmail("BBB@example.com");
+		salaryList.add(salaryModel);
+
+		
+		// 세 번째 데이터
+		salaryModel = new SalaryModel();
+		salaryModel.setId(3);
+		salaryModel.setName("CCC");
+		salaryModel.setEmail("CCC@example.com");
+		salaryList.add(salaryModel);
+		
+		
+		mav.addObject("salaryList",salaryList);
+		mav.setViewName("content/home");
 		
 		return mav;
 	}
