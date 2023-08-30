4bd1b591d0bfd303ec328fcca71ebfd347d3e70a
aknod <aknod@psy>
2023-08-30 14:23:01 +0900

    [Spring] Spring Boot 시작하기 (3) - Lombok 사용 기본 (Data, Builder, NonNull,Builder.Default)

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -17,42 +17,83 @@ import jakarta.servlet.http.HttpServletRequest;
 public class HomeController {
 	@RequestMapping(value = "/home", method=RequestMethod.GET)
 	public ModelAndView goHome(HttpServletRequest request) {
+		
 		ModelAndView mav = new ModelAndView();
-
+		
 		//salary 객체 리스트 변수
-		List<SalaryModel> salaryList = new ArrayList<SalaryModel>();
+		List<SalaryModel> salaryList = new ArrayList<SalaryModel>(); // 다형성 구현을 위해  도형 = 정사각형();이라고 정의한 것
 		
+		//salaryModel
+		SalaryModel salaryModel;
 		
-		//Salary model
-		SalaryModel salaryModel = new SalaryModel();
-		
-		// 첫 번째 데이터
-		salaryModel = new SalaryModel();
-		salaryModel.setId(1);
-		salaryModel.setName("AAA");
-		salaryModel.setEmail("AAA@example.com");		
+		//1.
+		salaryModel = SalaryModel.builder()
+				//.id(1)
+				.name("AAA")
+				.email("AAA@naver.com").build();
 		salaryList.add(salaryModel);
 		
-		
-		// 두 번째 데이터
-		salaryModel = new SalaryModel();
-		salaryModel.setId(2);
-		salaryModel.setName("BBB");
-		salaryModel.setEmail("BBB@example.com");
-		salaryList.add(salaryModel);
-
-		
-		// 세 번째 데이터
-		salaryModel = new SalaryModel();
-		salaryModel.setId(3);
-		salaryModel.setName("CCC");
-		salaryModel.setEmail("CCC@example.com");
+		//2.
+		salaryModel = SalaryModel.builder()
+				.id(2)
+				.name("BBB")
+				.email("BBB@naver.com").build();
 		salaryList.add(salaryModel);
 		
+		//3.
+		salaryModel = SalaryModel.builder()
+				.id(3)
+				.name("CCC")
+				.email("CCC@naver.com").build();
+		salaryList.add(salaryModel);
 		
-		mav.addObject("salaryList",salaryList);
-		mav.setViewName("content/home");
+		mav.addObject("salaryList", salaryList); // 데이터 키값으로 넘겨줌
+		mav.setViewName("content/home"); // 보여줄 view 설정
 		
 		return mav;
+		
 	}
 }
+//@Controller
+//public class HomeController {
+//	@RequestMapping(value = "/home", method=RequestMethod.GET)
+//	public ModelAndView goHome(HttpServletRequest request) {
+//		ModelAndView mav = new ModelAndView();
+//		
+//		//salary 객체 리스트 변수
+//		List<SalaryModel> salaryList = new ArrayList<SalaryModel>();
+//		
+//		
+//		//Salary model
+//		SalaryModel salaryModel = new SalaryModel();
+//		
+//		// 첫 번째 데이터
+//		salaryModel = new SalaryModel();
+//		salaryModel.setId(1);
+//		salaryModel.setName("AAA");
+//		salaryModel.setEmail("AAA@example.com");		
+//		salaryList.add(salaryModel);
+//		
+//		
+//		// 두 번째 데이터
+//		salaryModel = new SalaryModel();
+//		salaryModel.setId(2);
+//		salaryModel.setName("BBB");
+//		salaryModel.setEmail("BBB@example.com");
+//		salaryList.add(salaryModel);
+//		
+//		
+//		// 세 번째 데이터
+//		salaryModel = new SalaryModel();
+//		salaryModel.setId(3);
+//		salaryModel.setName("CCC");
+//		salaryModel.setEmail("CCC@example.com");
+//		salaryList.add(salaryModel);
+//		
+//		
+//		mav.addObject("salaryList",salaryList);
+//		mav.setViewName("content/home");
+//		
+//		return mav;
+//	}
+//}
--- demo/src/main/java/com/example/demo/model/SalaryModel.java
+++ demo/src/main/java/com/example/demo/model/SalaryModel.java
@@ -2,9 +2,9 @@ package com.example.demo.model;
 
 import lombok.*;
 
-@Data
+@Builder @Data
 public class SalaryModel {
-	private int id;
-	private String name;
-	private String email;	
+	@NonNull @Builder.Default private int id = 5; // 널 값 허용하지 않음, 디폴트 값을 설정해줌
+	@NonNull private String name;
+	@NonNull private String email;	
 }
